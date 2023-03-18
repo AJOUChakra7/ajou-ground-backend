3cf5ee354b451fa6a8363cbfab9e2b6ec1dd65c9
kljp <slashxp@naver.com>
2023-03-19 00:25:44 +0900

    Add function for finding reservation by date

--- routes/reservation.route.js
+++ routes/reservation.route.js
@@ -1,29 +1,52 @@
 const { Router } = require("express");
 const { findOneUserByEmail } = require("../lib/functions/user");
-const { createReservation } = require("../lib/functions/reservation");
+const { createReservation, findReservationByDate } = require("../lib/functions/reservation");
 const { findOneGroundByName } = require("../lib/functions/ground");
-const { catchAsync } = require("../lib/utils");
+const { catchAsync, ApiError } = require("../lib/utils");
+const httpStatus = require("http-status");
+// const { groupBy } = require('../lib/utils')
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const groupBy = (data, createKey) => {
+  return data.reduce((result, current) => {
+    const key = createKey(current);
+    const value = result[key];
+    if (value == null) {
+      result[key] = [current];
+    } else {
+      value.push(current);
+    }
+    return result;
+  }, {});
+}
+
+router.get("/", catchAsync(async (req, res) => {
+  const { date } = req.query
+
+  if (!date) {
+    throw new ApiError(httpStatus.BAD_REQUEST, "Input the date")
+  }
+
+  const reservations = await findReservationByDate(date)
+
+  res.json({ ...groupBy(reservations, ({ startTime }) => startTime.getHours() ) })
+}))
 
 router.post("/", catchAsync(async (req, res) => {
-    const { startTime, ground } = req.body;
+  const { startTime, ground } = req.body;
 
-    const user = await findOneUserByEmail("test@ajou.ac.kr");
-    const groundDoc = await findOneGroundByName(ground);
-    console.log(user, groundDoc)
+  const user = await findOneUserByEmail("test@ajou.ac.kr");
+  const groundDoc = await findOneGroundByName(ground);
+  console.log(user, groundDoc)
 
-    const reservation = await createReservation({
-      startTime,
-      user,
-      ground: groundDoc,
-    });
+  const reservation = await createReservation({
+    startTime,
+    user,
+    ground: groundDoc,
+  });
 
-    return res.json({ reservation });
+  return res.json({ reservation });
 }));
 
 module.exports = router;
