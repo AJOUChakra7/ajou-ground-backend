2510c8c071d82edac70672ad65bcb80fbfb8483a
Jiheon Choi <jiheon.unidev@gmail.com>
2023-03-18 20:58:14 +0900

    feat: Add api routes for reservation feature (#2)
    * Fix .gitignore
    * Add functions for catch error
    * Update functions for create row
    * Fix typo in schema
    * Add functions for error
    * Add routes for reservation

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('./lib/utils/config');
 const routes = require("./routes");
 const { makeConnectionToMongo } = require("./lib/utils/dbConnect");
+const { errorConverter, errorHandler } = require("./lib/middleware/error");
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", routes);
+
+app.use(errorConverter);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     makeConnectionToMongo({ url: config.dbUrl });
